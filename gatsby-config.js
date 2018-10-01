17ce7f634e267a5382829a6ce938fc53043afa84
Stephane Coué <steph.coue@gmail.com>
2018-10-01 05:20:23 +0200

    chore(cssInJs) Added emotion

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,6 +12,12 @@ module.exports = {
     },
     'gatsby-transformer-sharp',
     'gatsby-plugin-sharp',
+    {
+      resolve: `gatsby-plugin-emotion`,
+      options: {
+        // Accepts all options defined by `babel-plugin-emotion` plugin.
+      },
+    },
     'gatsby-plugin-react-helmet',
     {
       resolve: `gatsby-plugin-manifest`,
@@ -22,7 +28,7 @@ module.exports = {
         background_color: '#663399',
         theme_color: '#663399',
         display: 'minimal-ui',
-        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
+        icon: 'src/images/pisc-icon.png', // This path is relative to the root of the site.
       },
     },
     'gatsby-plugin-offline',
